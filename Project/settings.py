8517d013a311a7354777560d78ddfc08f4baa115
StephenOnyekachi <88212958+StephenOnyekachi@users.noreply.github.com>
2026-06-24 21:27:28 +0100

    Connect Django to Supabase

--- Project/settings.py
+++ Project/settings.py
@@ -102,19 +102,19 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# # for render deployment, use the following database configuration
 # DATABASES = {
-#     "default": dj_database_url.config(
-#         default=os.environ.get("DATABASE_URL")
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+# for render deployment, use the following database configuration
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 
 # Password validation
